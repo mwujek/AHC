661111855506fa5b9b9e0f8ef7502baba95f324a
Matt Wujek <mattwujek@gmail.com>
2014-08-21 02:13:06 -0700

    slideshow
    Added JS to make homepage slideshow
    need to add an arrow and ‘scroll down’ text

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 
+
 	//fullPage.js initialization
 	 $('#fullpage').fullpage({
         verticalCentered: true,
         resize : false,
         scrollingSpeed: 700,
         css3: true,
-        anchors: ['nav-1', 'nav-2', 'nav-3', 'nav-4', 'nav-5', 'nav-6', 'nav-7', 'nav-8', 'nav-9'],
+        anchors: ['nav-1', 'nav-2', 'nav-3', 'nav-4', 'nav-5', 'nav-6', 'nav-7', 'nav-8', 'nav-9', 'nav-10'],
     	menu: '#mainNav'
     });
 
@@ -121,5 +122,42 @@ $(document).ready(function(){
 	//make a div to center certain link buttons 
 	$('.button-center').wrap("<div class='link-wrapper'></div>");
 
+	
+
 
 });
+
+
+
+	function slideSwitch() {
+        var active = $('div.active-ss');
+        var nextSlide = active.next();
+        var lastSlide = $('#fourth');
+        var firstSlide = $('#first');
+        var fadeSpeed = 1000;
+        //console.log('time!'); 
+
+        
+        if(lastSlide.hasClass('active-ss') ===true){
+        //window.alert('YES!');
+        active.removeClass('active-ss');
+        firstSlide.addClass('active-ss');
+        active.fadeOut();
+        firstSlide.fadeIn();
+
+} else {
+        //if(active-ss ===)
+   		
+   		active.removeClass('active-ss');
+        nextSlide.addClass('active-ss');
+        active.fadeOut();
+        nextSlide.fadeIn();
+
+
+        }
+    }
+
+    	//slideshow for home page
+    $(function() {
+        setInterval(slideSwitch, 3000);
+    });
